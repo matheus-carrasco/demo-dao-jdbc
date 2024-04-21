37d59bf70d5989d70693a1d48d79fb367a5527c1
matheus-carrasco <matheuscarrasco02@gmail.com>
2024-04-20 23:53:03 -0300

    DepartmentDao and SellerDato interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,12 +12,8 @@ public class Program {
 		Department obj = new Department(1, "Books");		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
-		
 		System.out.println(obj);
-		System.out.println(seller);
-		
-		
-		
+		System.out.println(seller);		
 	}
 
 }
